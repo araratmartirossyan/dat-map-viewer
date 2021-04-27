b9545260359c74d87979e8a34d23c6782c023634
Ararat <ararat.martirossyan@jumbo.com>
2021-04-27 17:16:14 +0200

    Inited repo

--- scripts/element-fix.js
+++ scripts/element-fix.js
@@ -5,46 +5,25 @@ fs.rmdirSync(path.resolve(__dirname, '../node_modules/.vite_opt_cache'), {
   recursive: true
 })
 
-const localePath = path.resolve(
-  __dirname,
-  '../node_modules/element-plus/lib/index.d.ts'
-)
+try {
+  const transitionPath = path.resolve(
+    __dirname,
+    '../node_modules/element-plus/lib/el-collapse-transition/index.d.ts'
+  )
 
-fs.writeFileSync(
-  localePath,
-  fs.readFileSync(localePath, 'utf8').replace('./el-locale', './locale')
-)
+  console.log('TRANSITION PATH', transitionPath)
 
-const constPath = path.resolve(
-  __dirname,
-  '../node_modules/element-plus/lib/el-option/index.d.ts'
-)
+  fs.writeFileSync(
+    transitionPath,
+    fs
+      .readFileSync(transitionPath, 'utf8')
+      .replace(
+        '../transition/collapse-transition/index.vue',
+        '../el-transition/collapse-transition/index.vue'
+      )
+  )
 
-fs.writeFileSync(
-  constPath,
-  fs.readFileSync(constPath, 'utf8').replace('@element-plus', '..')
-)
-
-const dialogPath = path.resolve(
-  __dirname,
-  '../node_modules/element-plus/lib/el-dialog/src/useDialog.d.ts'
-)
-
-fs.writeFileSync(
-  dialogPath,
-  fs.readFileSync(dialogPath, 'utf8').replace('/el-utils/', '/utils/')
-)
-
-const transitionPath = path.resolve(
-  __dirname,
-  '../node_modules/element-plus/lib/el-collapse-transition/index.d.ts'
-)
-
-fs.writeFileSync(
-  transitionPath,
-  fs
-    .readFileSync(transitionPath, 'utf8')
-    .replace('../transition/', '../el-transtion/')
-)
-
-console.log('DONE')
+  console.log('DONE')
+} catch (err) {
+  console.log(err)
+}
